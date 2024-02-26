2e2f5383171548c3494919e969d6d5e41c054023
robin-hinks <robin.e.hinks@gmail.com>
2024-02-26 15:57:17 -0800

    checked data types, added to-do's for cleaning

--- code/1_data_prep.R
+++ code/1_data_prep.R
@@ -1,1 +1,9 @@
-raw_data = read.csv("CRLF_EGG_RAWDATA.csv")
+library(tidyverse)
+raw_data <- read_csv("CRLF_EGG_RAWDATA.csv")
+
+#checking type of each column
+str(raw_data)
+
+#many ints in Survey_MONTH are empty but the values in Date look to be full. need to get month from Date
+
+#maybe we should have survey length, width, water depth, etc as 1 standardized value for each pond per breeding year? Not sure how we want to do that
