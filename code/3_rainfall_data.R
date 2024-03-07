82b11b5a2cba8b81bbec699984e37ae8a5f5fec7
robin-hinks <robin.e.hinks@gmail.com>
2024-03-07 12:58:35 -0800

    worked on rainfall data

--- code/3_rainfall_data.R
+++ code/3_rainfall_data.R
@@ -4,9 +4,19 @@ library(readxl)
 
 setwd(here::here("code"))
 
+# reading in muir woods data
 muwo_rain <- read_excel(here::here("data", "muwo_rain.xlsx"))
 
+#reading in corte madera data
 marin_rain_folder <- here::here("data", "MMWD_RainfallRecords2")
 rain_files <- list.files(path = marin_rain_folder, pattern = "\\.xlsx$", full.names = TRUE)
-rain_df_list <- lapply(rain_files, read_excel) # creates list of data frames (one data frame = one spreadsheet)
-rain_df <- bind_rows(rain_df_list, .id = "id")
+sheet_name = "CM"
+rain_data_list <- map(rain_files, ~read_excel(.x, sheet = sheet_name) %>%
+                        set_names(c("dayOfMonth", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "dayOfMonthAgain"))) #creates list of data frames, one for each year
+
+rain_data_list <- map(rain_data_list, ~mutate(.x, across(where(is.numeric), 
+                                                         ~ as.Date(.x, origin = "1899-12-30")))) # DOES NOT WORK YET!! date formatting is weird :(
+
+rain_data <- rain_data_list %>% bind_rows(.id = "File")
+
+#extract monthly rain data from rain_data
