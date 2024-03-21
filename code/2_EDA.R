af758f237849ee1b5e6556402c4003ecc5548242
孙祥荣 <sunxiangrong@wifi-10-44-136-5.wifi.berkeley.edu>
2024-03-21 00:05:40 -0700

    new changes

--- code/2_EDA.R
+++ code/2_EDA.R
@@ -48,7 +48,8 @@ abundance_counts <- data %>%
   summarize(Count = n_distinct(EventGUID))
 abundance_counts
 
-#only look at watersheds with decent amount of data?
+#Plot showing number of surveys per year
+#only at watersheds with decent amount of data (7 watersheds in total) 
 rich_watersheds <-abundance_counts|>
   filter(Watershed == "Kanoff Creek" | Watershed == "Laguna Salada" | Watershed =="Milagra Creek"|
            Watershed == "Redwood Creek" | Watershed == "Rodeo Lagoon" | Watershed=="Tennessee Valley" |
@@ -74,26 +75,22 @@ sum(new_total_egg_masses, na.rm = TRUE)
 summary(new_total_egg_masses,na.rm = TRUE)
 new_egg$BRDYEAR <- factor(new_egg$BRDYEAR)
 
-
-#plots of mean number of new egg masses per survey across all watersheds through time
-ggplot(data = new_egg)+ 
-  stat_summary(mapping = aes(x = BRDYEAR, y = NumberofEggMasses),
-               fun = "mean",geom = "point",color = "blue", size= 2)+
-  labs(x = "Year", y = "mean number of new egg masses")
- 
-#boxplot of above graph (y-axis is square-rooted for visualization)
-ggplot(data = new_egg, mapping = aes(x = BRDYEAR, y = sqrt(NumberofEggMasses)))+
-  geom_boxplot()+
-  labs(x = "Year", y = "sqrt of mean number of new egg masses")
-
 #stats for 1.number of survey, 2. mean number of new egg masses per survey, and 3.
-#total number of egg masses by watershed by site by year
-#mean number may be wrong (mean number of new egg masses per survey, i used old survey count)
+#total number of egg masses per watershed per site per year
 statistics <-new_egg|>
   group_by(BRDYEAR,Watershed,LocationID)|>
   summarize(count = n_distinct(EventGUID),
-            mean_num = mean(NumberofEggMasses, na.rm = TRUE),
-            total_num =sum(NumberofEggMasses, na.rm = TRUE))
+            mean_num = (sum(NumberofEggMasses, na.rm = TRUE)/count),
+            total_num =sum(NumberofEggMasses, na.rm = TRUE),
+            mean_depth = mean(AvgD))
+statistics
+
+#plots of mean number of new egg masses per survey across all watersheds of all years
+ggplot(data = statistics)+ 
+  stat_summary(mapping = aes(x = BRDYEAR, y = mean_num),
+               fun = "mean",geom = "point",color = "blue", size= 2)+
+  labs(x = "Year", y = "mean number of new egg masses")
+
 
 #above graph only looking at 7 target watersheds, and filter by having at least 2 new egg masses per year
 statistics_rich <- statistics |>
@@ -103,30 +100,27 @@ statistics_rich <- statistics |>
   filter(count > 1)
 statistics_rich$count <- as.factor(statistics_rich$count)
 
-#plot of total egg masses over time per site
+#plot of total egg masses over time per site at the 7 "rich"watersheds
 ggplot(data = statistics_rich, mapping = aes(x = BRDYEAR, y = total_num)) +
   geom_point(aes(size = count), alpha = 1/2) + facet_wrap(~LocationID) +
   labs(x = "Year", y = "total new egg masses")
   
-
-#plot of mean egg masses over time by site
+#plot of mean egg masses over time per site at the 7 "rich"watersheds
 ggplot(data = statistics, mapping = aes(x = BRDYEAR, y = mean_num)) +
   geom_point(alpha = 1/3) + facet_wrap(~LocationID)+
   labs(x = "Year", y = "mean new egg masses")
 
-#example: showing the number of survey for all sites in Redwood Creek watershed in 2016
+#example: number of survey for all sites in Redwood Creek watershed in 2016
 data|>
   filter(Watershed=="Redwood Creek")|>
   filter(BRDYEAR == "2016")|>
-  group_by(Watershed,LocationID)|>
+  group_by(Watershed,LocationID)|>.
   summarize(survey_count=n_distinct(EventGUID))
 
-
-#number of survey for each number of new egg masses of all watersheds by year
-ggplot(data = new_egg, mapping = aes(x = NumberofEggMasses)) + 
+#number of survey count for each number of new egg masses of all watersheds by year
+ggplot(data = statistics, mapping = aes(x = total_num)) + 
   geom_freqpoly(mapping = aes(colour = BRDYEAR),bins = 4)+
-  scale_x_continuous(limits = c(0, 3))
-
+  scale_x_continuous(limits = c(0, 200))
 
 #table showing number of different sites for all watersheds
 number_of_sites_within_watershed <- data|>
@@ -138,35 +132,44 @@ number_of_sites_within_watershed
 ggplot(number_of_sites_within_watershed,aes(x=Watershed,y=distinct_count))+
   geom_point()
 
-
-#histogram of response data for all years pooled (may change x-axis, not that helful)
-ggplot(data = data, aes(x = NumberofEggMasses)) + geom_histogram()
-
-n_distinct(raw_data$EventGUID)
-
 #counting the number of non-NA values for the environmental variables we are looking at 
 data$WaterSalinity
 non_na_Salinity <- na.omit(data$WaterSalinity)
 length(non_na_Salinity)
 sum(non_na_Salinity != 0)
+#there are 1200 salinity data, only 92 of them is non-zero. 
 
 data |>
   filter(data$WaterSalinity>0)|>
   group_by(Watershed)|>
   summarise(count=n())
+#the distribution of these92 non-zero salinity data by watershed.
 
 data$PercentOpenWater
 non_na_open <- na.omit(data$PercentOpenWater)
 length(non_na_open)
 sum(non_na_open != 0)
+#there are 4937 non-zero open water data
 
 data$AvgD
 non_na_depth <- na.omit(data$AvgD)
 length(non_na_depth)
+#there are 3404 non-zero open water data
 
 data$MaxD
 non_na_depth2 <- na.omit(data$MaxD)
 length(non_na_depth2)
+#there are 5091 non-zero open water data
+
+#correlation between average depth and max depth is significantly positive 
+regression_depth=lm(AvgD~MaxD,data=new_egg)
+summary(regression_depth)
+
+
+ggscatter(new_egg, x = "MaxD", y = "AvgD",
+          add = "reg.line",cor.coef=TRUE,coor.method=" ")
+
+
 
 # investigating number of observers (obsv_total)
 ## histogram of obsv_total
