eeedc93876a59e44800c3acfa8a783800de59c83
robin-hinks <robin.e.hinks@gmail.com>
2024-03-21 20:59:16 -0700

    exported rainfall as csvs

--- code/1_data_prep.R
+++ code/1_data_prep.R
@@ -2,14 +2,15 @@ library(tidyverse)
 library(dplyr)
 library(here)
 library(lubridate)
-source("3_rainfall_data.R") #TODO: change this to read in csv from rainfall data
 
 setwd(here::here("code"))
 raw_data <- read_csv(here::here("data", "CRLF_EGG_RAWDATA.csv"))
+rainfall_daily <- read_csv(here::here("data", "cm_daily_rain.csv"))
+rainfall_yearly <- read_csv(here::here("data", "cm_yearly_rain.csv"))
 
 # removing unnecessary columns, making new column for total vegetation (to make sure it adds to 100), making data types more accurate/easier to use
 # the DATA variable that this pipe generates has all validated rows and has not been filtered
-# filtered data is denoted below this, and will always use DATA as a starting point
+# filtered data is denoted below this, and uses DATA as a starting point
 data <- raw_data %>% select(-ParkCode, -ProjectCode, -BTime, -TTime, -USGS_ID, -SEASON, -SvyLength, -SvyWidth, -tblEvents.Comments, 
                             -DateEntered, -EventID, -SpeciesID, -WaterDepth, -EggDepth, -Distance, -EggMassStageID, -AS_UTMSOURCE, -AS_UTMZONE, 
                             -GPS_ID, -tblEggCount_CRLF.Comments, -RangeofEggMasses, -AttachType) %>% 
@@ -34,6 +35,11 @@ data <- raw_data %>% select(-ParkCode, -ProjectCode, -BTime, -TTime, -USGS_ID, -
 total_observations <- data %>% group_by(EggCountGUID) %>% summarise(Obsv_Total = sum(!is.na(Obsv1),!is.na(Obsv2),!is.na(Obsv3)))
 
 data$obsv_total <- total_observations$Obsv_Total
+
+### ~~~ *** INCORPORATING RAINFALL DATA *** ~~~ ###
+data %>% mutate(
+  wy_rain <- rainfall_yearly$yearly_rain
+)
 
 ### ~~~ *** DATA FILTERING *** ~~~ ###
 
--- code/3a_rainfall_comparisons.R
+++ code/3a_rainfall_comparisons.R
@@ -5,6 +5,53 @@
 # that's ok, and by design (for now). the goal is to get 3_rainfall_data_prep.R to output
 # a CSV file that can be used for EDA here
 
+# reading in half moon bay data
+hmb_rain <- read_csv(here::here("data", "hmb_rain.csv")) %>% 
+  select("DATE", "PRCP") %>% 
+  mutate(DATE = trimws(DATE), DATE = ym(DATE)) %>% 
+  rename(monthly_rain = PRCP, date = DATE) %>% 
+  mutate(
+    beginningWY = case_when(
+      month(date) > 9 ~ floor_date(date, unit = "year") + months(9),
+      TRUE ~ floor_date(date, unit = "year") - months(3) # gets the beginning of the water year for each date 
+    ),
+    Water_Year = as.numeric(year(beginningWY)) + 1) %>% 
+  select(-beginningWY) %>% 
+  mutate(month = format(date, "%b"))
+
+# reading in muir woods data
+muwo_rain <- read_excel(here::here("data", "muwo_rain.xlsx")) %>% 
+  mutate(Oct = as.double(Oct), Nov = as.double(Nov), Dec = as.double(Dec), Jan = as.double(Jan), Feb = as.double(Feb), 
+         Mar = as.double(Mar), Apr = as.double(Apr), May = as.double(May), Jun = as.double(Jun), Jul = as.double(Jul), 
+         Aug = as.double(Aug), Sep = as.double(Sep))
+
+# combine into one monthly rain table for all locations
+temp_monthly_rain_table <- inner_join(hmb_rain, cm_rain, by = "date", suffix = c(".hmb", ".cm")) %>% 
+  select(Water_Year.hmb, month.hmb, monthly_rain.hmb, monthly_rain.cm) %>% 
+  rename(Water_Year = Water_Year.hmb, month = month.hmb, hmb_monthly_rain = monthly_rain.hmb, cm_monthly_rain = monthly_rain.cm)
+
+full_monthly_rain_table <- inner_join(temp_monthly_rain_table, muwo_monthly_rain, by = c("Water_Year", "month")) %>% 
+  rename(muwo_monthly_rain = monthly_rain)
+
+# for yearly rain -- all locations
+yearly_rain_cm_table <- cm_rain %>% 
+  group_by(Water_Year) %>% 
+  summarise(yearly_rain_cortemadera = sum(monthly_rain))
+
+yearly_rain_hmb_table <- hmb_rain %>% 
+  group_by(Water_Year) %>% 
+  summarise(yearly_rain_halfmoonbay = sum(monthly_rain, na.rm = TRUE))
+
+rain_to_compare_wide <- merge(merge(yearly_rain_cm_table, muwo_rain, all = TRUE), yearly_rain_hmb_table, all = TRUE) %>% 
+  select(Water_Year, yearly_rain_cortemadera, TOTALS, yearly_rain_halfmoonbay) %>% 
+  rename(corte_madera = yearly_rain_cortemadera, muir_woods = TOTALS, half_moon_bay = yearly_rain_halfmoonbay) %>% 
+  filter(Water_Year > 1998, Water_Year < 2022)
+
+rain_to_compare <- rain_to_compare_wide %>% pivot_longer(cols = 2:4, names_to = "location", values_to = "rainfall")
+
+rain_to_compare_after_missing_data <- rain_to_compare_wide %>% 
+  filter(Water_Year > 2003)
+
 # plot rainfall by location
 ggplot(data = rain_to_compare, aes(x = Water_Year, y = rainfall, color = location)) + geom_line()
 
