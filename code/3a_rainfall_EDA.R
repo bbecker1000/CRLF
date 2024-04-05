7229c4807fa02b45723be2936c4b55e86277939c
孙祥荣 <sunxiangrong@wifi-10-44-136-170.wifi.berkeley.edu>
2024-04-05 10:29:00 -0700

    new

--- code/3a_rainfall_EDA.R
+++ code/3a_rainfall_EDA.R
@@ -23,6 +23,8 @@ ggplot(data = rainfall_daily_long, aes(x = day_of_year, y = rainfall, color = fa
 
 ggplot(data = rainfall_daily_long, aes(x = day_of_year, y = rainfall)) + 
   geom_point(alpha = 0.1) + geom_smooth(method = "loess", color = "red4", se = FALSE, size = 2) +
+  geom_vline(xintercept = 60,linetype = 2)+
+  geom_vline(xintercept = 120,linetype = 2)+
   scale_y_continuous(trans = "log10") +
   facet_wrap(~Water_Year)
   
