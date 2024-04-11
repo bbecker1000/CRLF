1a28ce8ea68f9f4d1911645640a28b0df54d935f
robin-hinks <robin.e.hinks@gmail.com>
2024-04-10 19:47:03 -0700

    tried to troubleshoot model...

--- code/4b_within_year_analysis.R
+++ code/4b_within_year_analysis.R
@@ -2,13 +2,18 @@ library(here)
 library(sjPlot)
 library(lme4)
 library(glmmTMB) #instead of lme4
+library(lme4)
 library(cowplot)
 
 source("1_data_prep.R")
 
 # onset_of_breeding is the relevant table here
 
-within_year_model <- glmmTMB(first_breeding ~ rain_to_date + (1|LocationID), family = Gamma, data = onset_of_breeding)
+# I am getting weird errors when I try to use lme4 or glmmTMB... not sure if I'm doing something wrong here?
+# stack overflow says the glmmTMB error could also be because of the Matrix error...
+
+within_year_model <- glmmTMB(first_breeding ~ rain_to_date + MaxD + (1|LocationID), family = gaussian, data = onset_of_breeding)
+within_year_model <- lmer(first_breeding ~ rain_to_date + MaxD + (1|LocationID), data = onset_of_breeding)
 
 summary(within_year_model)
 
