1b502e3bc7a050c9676c1172b3c1c0757408154e
孙祥荣 <sunxiangrong@wifi-10-44-138-115.wifi.berkeley.edu>
2024-04-12 10:00:17 -0700

    1

--- code/4b_within_year_analysis.R
+++ code/4b_within_year_analysis.R
@@ -4,20 +4,23 @@ library(lme4)
 library(glmmTMB) #instead of lme4
 library(lme4)
 library(cowplot)
+library(lmerTest)
 
 source("1_data_prep.R")
 
 # onset_of_breeding is the relevant table here
 
 # the internet said Gamma is the correct family to use for timing variables, but I'm not attached to it if anyone has other ideas
-within_year_model <- glmmTMB(first_breeding ~ rain_to_date + MaxD + (1|LocationID), family = Gamma, data = onset_of_breeding)
-within_year_model_lmer <- lmer(first_breeding ~ rain_to_date + MaxD + (1|LocationID), data = onset_of_breeding)
+within_year_model <- glmmTMB(first_breeding ~ rain_to_date + (1|LocationID),family = Gamma, data = onset_of_breeding)
+within_year_model_lmer <- lmer(first_breeding ~ rain_to_date  + (1|LocationID), data = onset_of_breeding)
 
 summary(within_year_model)
 summary(within_year_model_lmer)
 
 plot_model(within_year_model, type = "diag")
 plot_model(within_year_model_lmer, type = "diag")
+
+cor.test(onset_of_breeding$rain_to_date,onset_of_breeding$MaxD)
 
 
 p.raw <- ggplot(onset_of_breeding, aes(x = first_breeding, y = rain_to_date, color = Watershed)) +
