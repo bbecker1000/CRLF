5c29b4d7877949a49cfc14ab959f895245b639bd
Becker <benbecker@nps.gov>
2024-04-18 10:30:24 -0700

    surv model updates

--- code/4c_within_year_survival.R
+++ code/4c_within_year_survival.R
@@ -7,16 +7,35 @@ library("survminer")
 
 #rename file
 onset_of_breeding_surv <- onset_of_breeding
+
+onset_of_breeding_surv$MaxD
+
+onset_of_breeding_surv$MaxD_f <- as.factor(ifelse(onset_of_breeding_surv$MaxD <0.5, "L", 
+                                                  ifelse(onset_of_breeding_surv$MaxD >=0.5 & onset_of_breeding_surv$MaxD < 1, "ML",      
+                                                         ifelse(onset_of_breeding_surv$MaxD >=1 & onset_of_breeding_surv$MaxD < 1.5, "MH",
+                                                                "H"))))
 #assign "dead" to all known breeders.  no censoring.
 onset_of_breeding_surv$status <- 2 
+
+d.rw <- onset_of_breeding_surv %>% filter(Watershed=="Redwood Creek")
+
 #intercept model of the mean
 fit.null <- survfit(Surv(rain_to_date, status) ~ 1, data = onset_of_breeding_surv)
 #survival (breeding probability) curves by watershed
-fit.watershed <- survfit(Surv(rain_to_date, status) ~ Watershed, data = onset_of_breeding_surv)
+fit.watershed <- survfit(Surv(rain_to_date, status) ~ Watershed + MaxD_f, data = onset_of_breeding_surv)
+
+fit.watershed.rw <- survfit(Surv(rain_to_date, status) ~ Watershed + MaxD_f, data = d.rw)
+
+
+fit.depth <- survfit(Surv(MaxD, status) ~ 1, data = onset_of_breeding_surv)
+
+
 
 #pick one to inspect/plot
 fit <- fit.null
 fit <- fit.watershed
+fit <- fit.depth
+fit <- fit.watershed.rw
 
 print(fit)
 # Summary of survival curves
@@ -38,13 +57,13 @@ d
 ggsurvplot(
   fit,                     # survfit object with calculated statistics.
   pval = TRUE,             # show p-value of log-rank test.
-  conf.int = TRUE,         # show confidence intervals for 
+  conf.int = FALSE,         # show confidence intervals for 
   risk.table = TRUE,       # Add risk table
   risk.table.col = "strata",
   conf.int.style = "step", #or ribbon
-  xlab = "Culumative rainfall",   # customize X axis label.
+  xlab = "Depth",   # customize X axis label.
   ylab = "p(breeding)",
-  break.time.by = 5,      # break X axis in time intervals by 200.
+  break.time.by = 0.25,      # break X axis in time intervals by 200.
   ggtheme = theme_light(), # customize plot and risk table with a theme.
   surv.median.line = "hv",  # add the median survival pointer.
   fun = "event"             #flips plot to culumative probability
