01dac3152c1f4715d9bc4fc9fd941bdaff99067e
dblavitt <dblavitt@gmail.com>
2024-05-02 22:46:38 -0700

    starting to set up between year model. not running. missing salinity.

--- code/4a_between_year_analysis.R
+++ code/4a_between_year_analysis.R
@@ -1,4 +1,43 @@
-library(here)
+setwd(here::here("code"))
 source("1_data_prep.R")
+library("glmmTMB")
+
+between_year_data <- read_xlsx(here::here("data", "between_year_data.csv"))
 
 # lmer or glmmTMB model to plot this
+
+## defining covarites from between year data
+egg_mass <- between_year_data$num_egg_masses
+year <- between_year_data$BRDYEAR
+
+emerg_veg <- between_year_data$mean_percent_emerg
+sub_veg <- between_year_data$mean_percent_sub
+water <- between_year_data$mean_percent_water
+canopy <- between_year_data$interpolated_canopy
+
+rain <- between_year_data$yearly_rain
+mean_max_depth <- between_year_data$mean_max_depth
+max_depth <- between_year_data$max_depth
+
+temp_air <- between_year_data$AirTemp
+temp_water <- between_year_data$WaterTemp
+
+watershed <- between_year_data$Watershed
+site <- between_year_data$LocationID
+
+# initial model -- MISSING SALINITY
+model1 <- glmmTMB(egg_mass ~ year + 
+                    emerg_veg + 
+                    sub_veg +
+                    water +
+                    canopy +
+                    rain + #total annual rainfall
+                    mean_max_depth +
+                    max_depth +
+                    temp_air +
+                    temp_water +
+                    (1|watershed/site) +
+                    ar(1),
+                  data = between_year_data,
+                  family = poisson) #autoregressive (temporal autocorrelation)
+summary(model1)
