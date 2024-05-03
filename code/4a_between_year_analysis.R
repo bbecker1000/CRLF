97b2a0d995dc8488fd55ffe0987e06b1a37fe3cf
robin-hinks <robin.e.hinks@gmail.com>
2024-05-03 10:35:40 -0700

    between year model updates

--- code/4a_between_year_analysis.R
+++ code/4a_between_year_analysis.R
@@ -2,6 +2,7 @@ library("glmmTMB")
 library(tidyverse)
 library(here)
 library("bbmle") 
+library(lme4)
 setwd(here::here("code"))
 
 between_year_data <- read_csv(here::here("data", "between_year_data.csv"))
@@ -29,27 +30,45 @@ between_year_data <- read_csv(here::here("data", "between_year_data.csv"))
 
 # alternatively: dplyr pipe so that all these variables can be in the same data frame
 
-
+hist(complete_btw_data$num_egg_masses, n = 150)
 #### initial model -- MISSING SALINITY ####
 model1 <- glmmTMB(num_egg_masses ~ BRDYEAR + 
-                    mean_percent_emerg + 
-                    mean_percent_sub +
-                    mean_percent_water +
-                    interpolated_canopy +
-                    yearly_rain + #total annual rainfall
-                    mean_max_depth +
-                    max_depth +
-                    AirTemp +
-                    WaterTemp +
-                    mean_salinity:CoastalSite +
-                    max_salinity:CoastalSite +
+                    # mean_percent_emerg + 
+                    # mean_percent_sub +
+                    # mean_percent_water +
+                    # interpolated_canopy +
+                    # yearly_rain + #total annual rainfall
+                    # mean_max_depth +
+                    # max_depth +
+                    # AirTemp +
+                     WaterTemp +
+                    # mean_salinity:CoastalSite +
+                    # max_salinity:CoastalSite +
                     (1 | Watershed) +
                     (1 | LocationID),
                   data = between_year_data,
                   ziformula = ~1,
-                  family = poisson) 
+                  family = nbinom2) 
 summary(model1)
 
+model1 <- glmer(num_egg_masses ~ I(BRDYEAR - 2009) + 
+                    # mean_percent_emerg + 
+                    # mean_percent_sub +
+                    mean_percent_water +
+                    # interpolated_canopy +
+                    yearly_rain + #total annual rainfall
+                    mean_max_depth +
+                    # max_depth +
+                    AirTemp +
+                    WaterTemp +
+                    # mean_salinity:CoastalSite +
+                    # max_salinity:CoastalSite +
+                    (1 | Watershed) +
+                    (1 | LocationID),
+                  data = between_year_data,
+                  # ziformula = ~1,
+                  family = negative.binomial(1)) 
+summary(model1)
 #### COMPLETE CASES ####
 # creating a "complete case" column
 between_year_data$complete_case <- complete.cases(between_year_data)
