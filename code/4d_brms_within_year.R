d8163f56117bb3d1b190304c688197e39d3984ad
bbecker1000 <bbecker@berkeley.edu>
2024-06-14 17:20:23 -0700

    added hurdle model

--- code/4d_brms_within_year.R
+++ code/4d_brms_within_year.R
@@ -10,8 +10,10 @@ mod.brm <- brm(bf(num_egg_masses ~  #bf creates a model statement for compilatio
                       s(WaterTemp_scaled) +  
                       CoastalSite + #not smoothed
                       (1|Watershed/LocationID),
-               zi ~ yearly_rain_scaled),
+               # hu ~ yearly_rain_scaled), #hurdle model for zeros
+               zi ~ yearly_rain_scaled),   #inflated model for zeros
                data = scaled_between_year,
+               # family = hurdle_negbinomial(),
                family = zero_inflated_negbinomial(),
                chains = 2, cores = 2,
                control = list(adapt_delta = 0.98)) #reduce divergences
@@ -20,11 +22,7 @@ summary(mod.brm)
 #pairs(mod.brm)
 conditional_effects(mod.brm, surface = FALSE, prob = 0.8)
 
-
-
-
-#for forest plot
-devtools::install_github("mvuorre/brmstools")
-library(brmstools) #from github
-forest(mod.brm)
+#try some plots with 
+library(tidybayes)
+#search tidybayes + brms for vignette
 
