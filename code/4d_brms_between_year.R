7efb2977a1530fb8fee473c9ba249be8aee3d0c6
bbecker1000 <bbecker@berkeley.edu>
2024-06-22 10:17:51 -0700

    Updated hurdle model to include priors

--- code/4d_brms_between_year.R
+++ code/4d_brms_between_year.R
@@ -30,33 +30,53 @@ t1-t0 # zi-model run time
 
 ## hurdle model ##
 t2 <- Sys.time()
-t2
+
+#set priors for problematic covariates
+#could add more prioris if helpful
+bprior <- c(prior(student_t(1, 0.5, 0.5), #slightly positive based on prior knowledge
+                  coef = syearly_rain_scaled_1),
+            prior(student_t(1, 0.5, 0.5),  #slightly positive based on prior knowledge
+                  coef = syearly_rain_scaled:water_regimeperennial_1),
+            prior(student_t(1, 0, 0.5), 
+                  coef =  syearly_rain_scaled:water_regimeseasonal_1),
+            prior(student_t(1, 0, 0.5), 
+                  coef =  smax_salinity_scaled:CoastalSiteFALSE_1),
+            prior(student_t(1, -0.25, 0.5),  #slightly negative based on prior knowledge
+                  coef =  smax_salinity_scaled:CoastalSiteTRUE_1)
+)
+
 
 mod.hurdle <- brm(
   bf(num_egg_masses ~ 
        s(BRDYEAR_scaled) + 
        s(yearly_rain_scaled) +
        s(mean_percent_water_scaled) + 
+       CoastalSite +  #new
+       water_regime + #new
        s(interpolated_canopy_scaled) +
        s(WaterTemp_scaled) +  
-       (max_salinity_scaled * CoastalSite) + # not smoothed
-       s(yearly_rain_scaled * water_regime) + # smooth or not?
+       s(max_salinity_scaled, by = CoastalSite) + # not smoothed
+       s(yearly_rain_scaled, by = water_regime) + # smooth or not?
        (1 | Watershed/LocationID),
-     hu ~ yearly_rain_scaled +      # inflated model for zeros
+     hu ~ s(yearly_rain_scaled, by = water_regime) + #new water regime for hurdle 
        (1|Watershed/LocationID)),
   data = scaled_between_year,
+  prior = bprior, #set above before model
   family = hurdle_negbinomial(),
-  chains = 2, cores = 2,
-  iter = 14000, # needs more iterations with added covariates
-  control = list(adapt_delta = 0.98)) #reduce divergences
+  chains = 3, cores = 3,
+  iter = 5000, # needs more iterations with interactions
+  control = list(adapt_delta = 0.98,   #reduce divergences, 
+                 max_treedepth = 20))  #had treedepth exceedances at default 10
 
 save(mod.hurdle, file = "Output/mod.hurdle.RData")
 #load("Output/mod.hurdle.RData")
 summary(mod.hurdle)
 
 t3 <- Sys.time()
-t3
 t3-t2 # hurdle model run time
+
+#get coefficient names for priors
+prior_summary(mod.hurdle) 
 
 # pairs(mod.brm)
 conditional_effects(mod.brm, surface = FALSE, prob = 0.8)
